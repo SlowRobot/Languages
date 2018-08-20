ab40dfaa2ad683f80ca50c8b5597becc10cf863b
SlowRobot <z5114667@student.unsw.edu.au>
2018-08-20 12:05:44 +1000

    Added Chebyshev formula method

---
+++ C/chebyshev.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int dist(int col, int y, int num);
+int max(int a, int b);
+int abs(int a);
+
+int main(int argv, char * argc[]) {
+
+    int number = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &number);
+
+    number = 4 * number - 1;
+
+    for(int row = 0; row < number; row++) {
+        for(int col = 0; col < number; col++) {
+            if(dist(col, row, number)) {
+                printf("#");
+            } else {
+                printf("-");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int dist(int x, int y, int num) {
+
+    int dist = max(abs(x - num / 2), abs(y - num / 2));
+    if(dist % 2 == 0) {
+        if(num % 4 == 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+
+    }
+    if(num % 4 == 1) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+int max(int a, int b) {
+    if(a > b) {
+        return a;
+    }
+
+    return b;
+}
+
+int abs(int a) {
+    if(a < 0) {
+        a *= -1;
+    }
+    return a;
+}
